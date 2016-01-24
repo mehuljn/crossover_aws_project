37044b752bfa913ceb0bcfdf27adbc37ccfca867
mehuljn <mehul.jani@gmail.com>
2016-01-25 00:03:33 +0530

    fix 2 ext dns

--- source/00_run_aws.sh
+++ source/00_run_aws.sh
@@ -4,5 +4,5 @@ bash 01_setupnode_aws.sh
 bash 02_extract_MYBB.sh
 sudo mysql_secure_installation
 bash 03_ready_db.sh localhost 3306 root dbroot mybbuser password
-bash 04_config_mybb.sh root@localhost localhost 3306 mybbuser password
+bash 04_config_mybb.sh root@localhost localhost 3306 mybbuser password 127.0.0.1:8080
 sudo service httpd start
