570db8aa4be40cbbcc7e31a36246171f830d2be5
mehuljn <mehul.jani@gmail.com>
2016-01-25 02:44:34 +0530

    additional fix

--- source/00_run_aws_cf_single.sh
+++ source/00_run_aws_cf_single.sh
@@ -2,7 +2,6 @@
 
 bash 01_setupnode_aws.sh
 bash 02_extract_MYBB.sh
-sudo mysql_secure_installation
-bash 03_ready_db.sh localhost 3306 root dbroot mybbuser password
-bash 04_config_mybb.sh root@localhost localhost 3306 mybbuser password 127.0.0.1:8080
+bash 03_ready_db.sh
+bash 04_config_mybb.sh
 sudo service httpd start
